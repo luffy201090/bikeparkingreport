f3ca4a44ec070f2660860f4d9e6ac33e4eedb23a
Dinh Hung Cuong <d.luffy2010@gmail.com>
2018-10-17 16:13:45 +0700

    update code

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('dist/bikepark'));
+app.use(express.static(__dirname + 'dist/bikepark'));
 
 app.post('/bikepark', (req, res) => {
   const { startDate, endDate, name, amount } = req.body;
